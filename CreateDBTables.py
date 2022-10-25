5b382de4ea13c8915dafa1a0a08d62f0a57119b8
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2022-10-25 10:42:56 -0700

    Setting up CSV file

--- CreateDBTables.py
+++ CreateDBTables.py
@@ -49,19 +49,7 @@ createProduct = '''CREATE TABLE Product(
 )'''
 
 '''
-descriptions  = []
-benefits = []
-price = 0
-with open('PLANTS_database.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(row)
-    print(f'Processed {line_count} lines.')
+
 '''
 cursor.execute(createShop)
 print("Table created successfully")
---
+++ populateTables.py
@@ -0,0 +1,13 @@
+descriptions  = []
+benefits = []
+price = 0
+with open('PLANTS_database.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(row)
+    print(f'Processed {line_count} lines.')

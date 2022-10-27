d8f05237a13c23f7b4949e625566aa85942f7461
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2022-10-26 17:13:05 -0700

    Format data

--- CreateDBTables.py
+++ CreateDBTables.py
@@ -3,54 +3,122 @@ import csv
 import psycopg2
 
 #establishing the connection
+#Change X and Y to appropriate username and password
 conn = psycopg2.connect(
-   database="cse412", user='ENTER YOUR POSTGRES USERNAME', password='ENTER YOUR POSTGRES PASSWORD(if applicable)', host='127.0.0.1', port= '5432'
+   database="cse412", user='X', password='Y', host='127.0.0.1', port= '5432'
 )
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
 #Shop
 createShop = '''CREATE TABLE Shop(
-store_id INT NOT NULL PRIMARY KEY,
+store_id SERIAL PRIMARY KEY,
 phone_number CHAR(10) NOT NULL,
 store_address VARCHAR(100) NOT NULL
 )'''
-#Bill
-createBill = '''CREATE TABLE Bill(
 
-)'''
-#Payment
-createPayment = '''CREATE TABLE Payment(
 
-)'''
-#Wish List
-createWishlist = '''CREATE TABLE Wishlist(
-
-)'''
-#Customers
-createCustomer = '''CREATE TABLE Customer(
-
-)'''
-#Shopping Cart
-create Cart = '''CREATE TABLE Cart(
-
-)'''
-#Items
-createItems = '''CREATE TABLE Items(
-
-)'''
-#Feedback
-createFeedback = '''CREATE TABLE Feedback(
-
-)'''
 #Products
 createProduct = '''CREATE TABLE Product(
+product_id SERIAL PRIMARY KEY,
+product_code VARCHAR(5) NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+benefits CHAR(200),
+description CHAR(200),
+price FLOAT
+)'''
+
+#Payment
+createPayment = '''CREATE TABLE Payment(
+payment_id SERIAL PRIMARY KEY,
+payment_type CHAR(1) NOT NULL,
+cardholder_name CHAR(30) NOT NULL,
+amount FLOAT NOT NULL,
+card_number INT NOT NULL,
+cvv INT NOT NULL,
+expiry_date DATE NOT NULL
+)'''
+
+#Items
+createItems = '''CREATE TABLE Items(
+item_id SERIAL PRIMARY KEY,
+unit_price FLOAT NOT NULL,
+quantity INT NOT NULL,
+cart_id INT NOT NULL,
+product_id INT REFERENCES Product(product_id) NOT NULL
+)'''
+
+#Shopping Cart
+createCart = '''CREATE TABLE Cart(
+cart_id SERIAL PRIMARY KEY,
+customer_id INT REFERENCES Customer(customer_id) NOT NULL,
+total_amount FLOAT NOT NULL,
+item INT REFERENCES Items(item_id) NOT NULL
+)'''
+
+
+
+
+#Bill
+createBill = '''CREATE TABLE Bill(
+bill_id SERIAL PRIMARY KEY,
+payment_id INT REFERENCES Payment(payment_id) NOT NULL,
+customer_name CHAR(30) NOT NULL,
+total_amount FLOAT NOT NULL,
+item INT NOT NULL
+)'''
+
+#Wish List
+createWishlist = '''CREATE TABLE Wishlist(
+wishlist_id SERIAL PRIMARY KEY,
+list_name CHAR(50) NOT NULL,
+product_id INT REFERENCES Product(product_id) NOT NULL
+)'''
+
+#Customers
+createCustomer = '''CREATE TABLE Customer(
+customer_id SERIAL PRIMARY KEY,
+customer_name CHAR(30) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(50) NOT NULL,
+shipping_address VARCHAR(100) NOT NULL
 
 )'''
 
-'''
 
-'''
+#Feedback
+createFeedback = '''CREATE TABLE Feedback(
+feedback_id SERIAL PRIMARY KEY,
+comment VARCHAR(200) NOT NULL,
+customer_id INT REFERENCES Customer(customer_id) NOT NULL
+)'''
+
+
 cursor.execute(createShop)
-print("Table created successfully")
+print("Shop Table created successfully")
+
+cursor.execute(createProduct)
+print("Products Table created successfully")
+
+cursor.execute(createCustomer)
+print("Customer Table created successfully")
+
+cursor.execute(createFeedback)
+print("Feedback Table created successfully")
+
+cursor.execute(createItems)
+print("Items Table created successfully")
+
+cursor.execute(createCart)
+print("Cart Table created successfully")
+
+cursor.execute(createPayment)
+print("Payment Table created successfully")
+
+cursor.execute(createBill)
+print("Bill Table created successfully")
+
+cursor.execute(createWishlist)
+print("Wishlist Table created successfully")
+
 conn.commit()
--- populateTables.py
+++ populateTables.py
@@ -1,13 +1,30 @@
-descriptions  = []
-benefits = []
+import csv
+import random
+
+#Formatting data
+descriptions  = ["Alpine plant", "Desert terrain plant","Tropical plant", "Coastal terrain plant",
+                "Coniferous Tree", "Deciduous Tree", "Shrub", "Floral plant", "Fruit plant", "Indoor plant"]
+benefits = ["Therapeutic", "Decorative", "Fruity","Healing abilities"]
 price = 0
+data = []
 with open('PLANTS_database.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
+        data_row = []
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            print(row)
+            data_row.append(row[0]) #code
+            data_row.append(row[0+2]) #name
+            data_row.append(random.choice(benefits)) #benefit
+            data_row.append(random.choice(descriptions)) #description
+            data_row.append(round(random.uniform(1.00,20.00),2))#price
+            line_count += 1
+
+            print(data_row)
+
+            data.append(data_row)
+
     print(f'Processed {line_count} lines.')

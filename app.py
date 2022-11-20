1bae3662cf645887533b586bd6927e673277d4cc
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2022-11-19 17:03:27 -0700

    login successful

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,21 @@ def get_db_connection():
     )
     return conn
 
-
+def verify(username, password):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    getCount = ''' SELECT COUNT(*) FROM Customer
+    WHERE Customer.email = %s AND Customer.password = %s '''
+    row = [username,password]
+    cur.execute(getCount,row)
+    count = cur.fetchall()
+    cur.close()
+    conn.close()
+    print(count[0][0])
+    if(count[0][0] == 0):
+        return False
+    else:
+        return True
 #@app.route('/')
 #def hello():
     #return '<h1>Plant Store</h1>'
@@ -29,12 +43,29 @@ def getProducts():
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/account/<uname>/<password>')
+def account(uname,password):
+    status = verify(uname,password)
+    print(status)
+    if(status):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Product WHERE product_id < 101 ORDER BY product_id;')
+        prods = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('products.html', prods=prods)
+    else:
+        print("THERE ARE ERRORS")
+        return render_template('error.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+   if request.method == 'POST':
+      unm = request.form['username']
+      pwd = request.form['password']
+      return redirect(url_for('account',uname = unm,password=pwd))
+   else:
+      unm = request.form['username']
+      pwd = request.form['password']
+      return redirect(url_for('account',uname = unm,password=pwd))
